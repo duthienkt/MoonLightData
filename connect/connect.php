84ef9d288a64456fe8decbd152f98421242ab5ce
duthienkt <blueskythien2010@live.com>
2018-01-03 05:25:00 +0000

    Fix bug

--- connect/connect.php
+++ connect/connect.php
@@ -11,5 +11,5 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-mysqli_query($conn,"SET NAMES 'UTF8'");s
+mysqli_query($conn,"SET NAMES 'UTF8'");
 ?>
--- facebooklogin.php
+++ facebooklogin.php
@@ -1,61 +1,65 @@
 <?php
     session_start();
-    $status = "Fail";
-    if($_POST["gender"] == "Male")
-    $_POST["gender"] = "male";
-    if($_POST["gender"] == "Female")
-    $_POST["gender"] = "female";
-    
+    $status = 'Fail';
+    if ($_POST['gender'] == 'Male') {
+        $_POST['gender'] = 'male';
+    }
+    if ($_POST['gender'] == 'Female') {
+        $_POST['gender'] = 'female';
+    }ls 
+
     // $deviceId =  'qwertyuiop';
-    $link = mysqli_connect('localhost', 'root', '12345blue', 'moondb');
-    mysqli_set_charset($link,'utf8');
-    $sql = "SELECT * FROM `user` where email='".$_POST["email"]."';";
+         $link = mysqli_connect('localhost', 'root', '12345blue', 'moondb');
+    mysqli_set_charset($link, 'utf8');
+    $sql = "SELECT * FROM `user` where email='".$_POST['email']."';";
     $results = array();
-    $sqlresult = mysqli_query($link,$sql);
-    
-    if ($sqlresult){
-        $status = "OK";
-        while($r = mysqli_fetch_assoc($sqlresult)) {
+    $sqlresult = mysqli_query($link, $sql);
+
+    if ($sqlresult) {
+        $status = 'OK';
+        while ($r = mysqli_fetch_assoc($sqlresult)) {
             $results[] = $r;
         }
-    }  
-    if (count($results)<1){
-        $sql = "INSERT INTO `user` (`userName`, `fullName`, `email`, `password`, `sex`, `birthday`, `createDay`, `updateDay`, `coin`, `phone`) "
-            ."VALUES( '"
-            .$_POST["id"]."', '"
-            .$_POST["name"]."', '"
-            .$_POST["email"]."', 'NoPassword', '".$_POST["gender"]."', NOW(), NOW(),NOW(), 100, 'NoPhone')";
-  
-  //Get result
-        $sqlresult = mysqli_query($link,$sql);
-    
-        if ($sqlresult){
-            $status = "OK";
-            while($r = mysqli_fetch_assoc($sqlresult)) {
-                $results[] = $r;
-            }
-        }  
     }
     
-     $sql = "SELECT * FROM `user` where email='".$_POST["email"]."';";
-     $results = array();
-    $sqlresult = mysqli_query($link,$sql);
     
-    if ($sqlresult){
-        $status = "OK";
-        while($r = mysqli_fetch_assoc($sqlresult)) {
+    
+    if (count($results) < 1) {
+        $sql = 'INSERT INTO `user` (`userName`, `fullName`, `email`, `password`, `sex`, `birthday`, `createDay`, `updateDay`, `coin`, `phone`) '
+            ."VALUES( '"
+            .$_POST['id']."', '"
+            .$_POST['name']."', '"
+            .$_POST['email']."', 'NoPassword', '".$_POST['gender']."', NOW(), NOW(),NOW(), 100, 'NoPhone')";
+
+  //Get result
+        $sqlresult = mysqli_query($link, $sql);
+
+        if ($sqlresult) {
+            $status = 'OK';
+            while ($r = mysqli_fetch_assoc($sqlresult)) {
+                $results[] = $r;
+            }
+        }
+    }
+
+     $sql = "SELECT * FROM `user` where email='".$_POST['email']."';";
+     $results = array();
+    $sqlresult = mysqli_query($link, $sql);
+
+    if ($sqlresult) {
+        $status = 'OK';
+        while ($r = mysqli_fetch_assoc($sqlresult)) {
             $results[] = $r;
         }
-    }  
-    
-    
-    $_SESSION["userId"] =  $results[0]["iduser"];
-    
+    }
+
+    $_SESSION['userId'] = $results[0]['iduser'];
+
     $apiRes = array(
-        'status'=> $status,
-        'results'=> $results
+        'status' => $status,
+        'results' => $results,
         // 'sql'=>$sql
     );
-    print json_encode($apiRes);
-    // print json_encode($_POST);
+    echo json_encode($apiRes);
+    // print json_encode($_POST);;
 ?>
